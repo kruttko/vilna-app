e144410187bc58286c1b82295cc27441916c8bad
Alona Krutko <116527587+kruttko@users.noreply.github.com>
2023-07-07 11:41:31 -0700

    js change

--- js/apptentive-sdk.js
+++ js/apptentive-sdk.js
@@ -1,5 +1,12 @@
 (() => {
-    const button = ApptentiveSDK.engage("lol");
+    const contact = document.querySelector(".contact-me-button");
+    const btn = document.querySelector("button");
+
+    btn.addEventListener("click", function () {
+        contact.ApptentiveSDK.engage("lol");
+});
+
+    // const button = ApptentiveSDK.engage("lol");
 })();
 
 // (function init() {
