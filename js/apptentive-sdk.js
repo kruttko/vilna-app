0d8be27b0dd9abdca14b52e9165052618a16f673
Alona Krutko <116527587+kruttko@users.noreply.github.com>
2024-01-09 11:32:03 -0800

    MC trigger

--- js/apptentive-sdk.js
+++ js/apptentive-sdk.js
@@ -1,19 +1,35 @@
-(() => {
-    function engageEvent() {
-        document.getElementById('event').ApptentiveSDK.engage("lol");
-    }
+// const messageCenterBtn = document.querySelector("#btn-mc");
+// const survey1Btn = document.querySelector('.btn-survey-1');
+// const survey2Btn = document.querySelector('.btn-survey-2');
+// const noteBtn = document.querySelector('.btn-note');
 
-//     const btn = document.querySelector("button");
-
-//     btn.addEventListener("click", function () {
-//         ApptentiveSDK.engage("lol");
+// messageCenterBtn.addEventListener("click", () => {
+//     console.log('MC clicked');
 // });
 
-    // const button = ApptentiveSDK.engage("lol");
-})();
+// survey1Btn.addEventListener('click', () => {
+//     console.log('MC clicked');
+// });
 
-// (function init() {
-//     setTimeout(() => {
-//       ApptentiveSDK.engage('lol')
-//     }, 1000)
-//   })()
+// survey2Btn.addEventListener('click', () => {
+//     console.log('MC clicked');
+// });
+
+// noteBtn.addEventListener('click', () => {
+//     console.log('MC clicked');
+// });
+
+document.addEventListener("DOMContentLoaded", function() {
+    // Wait for the DOM to be fully loaded before adding event listeners
+  
+    // Assuming you have a button with the id "btn-mc" for Message Center
+    var messageCenterButton = document.getElementById("btn-mc");
+  
+    if (messageCenterButton) {
+      // Add a click event listener to the Message Center button
+      messageCenterButton.addEventListener("click", function() {
+        // Trigger the Message Center when the button is clicked
+        ApptentiveSDK.showMessageCenter();
+      });
+    }
+  });
